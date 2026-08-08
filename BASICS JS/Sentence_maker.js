7b1fef71944ff0ebc53df10252939f82ed4ebc8b
ThepapuCrusader <ach2020mozo@gmail.com>
2026-08-08 14:28:33 -0400

    Create Sentence_maker.js for story generation
    Added a sentence maker that generates two stories using different sets of words.

---
+++ BASICS JS/Sentence_maker.js
@@ -0,0 +1,21 @@
+let adjective = "red";
+let noun = "WE";
+let verb = "walk";
+let place = "PERU"
+let adjective2 = "blue";
+let noun2 = "they";
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory)
+
+adjective = "red"
+noun = "apple"
+verb = "kill"
+place = "CADIA"
+adjective2 = "heretic"
+noun2 = "MARINE"
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory)
