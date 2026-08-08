7da41ed80166317ad90c07fbd911469d0a6ebd1c
ThepapuCrusader <ach2020mozo@gmail.com>
2026-08-08 14:29:04 -0400

    Create Trivia_bot.js

---
+++ BASICS JS/Trivia_bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "warCrimeBot";
+let botLocation = "Germany";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "WUSSTEST DU, DASS MEINE LIEBLINGSSPRACHE " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "WUSSTEST DU " + favoriteLanguage + " DASS ES SICH UM EINE SPRACHE HANDELT, DIE FÜR DEN EINSATZ AUF WEBSEITEN ENTWICKELT WURDE.";
+console.log(codingFact);
+
+codingFact = "Wusstest du, dass " + favoriteLanguage + "in nur 10 Tagen entstanden ist?";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
