2ea9e92599a17642261d3fc907b20f24735bc8ba
ThepapuCrusader <ach2020mozo@gmail.com>
2026-08-09 13:44:00 -0400

    Add string case transformation examples
    Add examples for converting strings to upper and lower case.

---
+++ WORKING WITH STRINGS/Mayus_and_Minus.js
@@ -0,0 +1,15 @@
+//En caso de que quieres transformar un texto completo a minuscula o mayuscula JavaScript tiene metodos para estas situaciones.
+//toLowerCase() y toUpperCase() 
+
+//ejemplos
+
+let papu1 = "Goodbye World";
+let upperPapu = papu1.toUpperCase();
+console.log(upperPapu); //devuelve todo el texto en mayusculas 
+
+
+let papu2 = "Help me world";
+let lowerPapu = papu2.toLowerCase();
+console.log(lowerPapu); //devuelve todo el texto en minusculas
+
+//PD: estos dos metodos no modifican la cadena original, en su lugar crean una nueva
